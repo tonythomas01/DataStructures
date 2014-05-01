e1fc10705768180b28f9587c860d68b6f5701441
Tony Thomas <01tonythomas@gmail.com>
2014-05-02 00:54:17 +0530

    Added graph data structure

---
+++ graph/graph/graph.h
@@ -0,0 +1,22 @@
+#pragma once
+#include<iostream>
+#include<map>
+#include<string>
+#include<set>
+
+using namespace std;
+class Graph
+{
+    public:
+        map<int,set<int> > data;
+        bool graph_type;
+        void insert( int a, int b);
+        void print();
+        void delete_vertex(int a);
+        Graph(): graph_type(0) {}
+        Graph( bool b) : graph_type(b)  {}
+    protected:
+    private:
+};
+
+#include "graph.cpp"
